6f4c9cbcf3472b980d53573aa653cc44b209f808
dbinar-RMA <53895640+dbinar-RMA@users.noreply.github.com>
2019-11-18 15:32:15 +0100

    HTML update
    removed html tags, 
    they're supposed to programatically create themself for automation purposes

--- src/index.js
+++ src/index.js
@@ -4,36 +4,77 @@ import "./styles.css";
 function compareImages() {
   var clicked = 0;
   var pos = 0;
+  var img = new Image();
   var div_container = document.getElementById("div-container"); // the main div
-  var div_regular = document.getElementById("div-regular"); // the unmoved div
+  var div_regular = createFixedElement(); //document.getElementById("div-regular"); // the unmoved div
   var cWidth = div_regular.offsetWidth;
-  var div_slider = document.getElementById("div-slider"); // div containing the slider
-  // set slider container position
-  div_slider.style.left =
-    (div_regular.offsetWidth - div_slider.offsetWidth / 2) / 2 + "px";
-  div_slider.style.top = cWidth / 4 + "px";
-  div_slider.style.zIndex = "3";
-  // set events
-  div_slider.addEventListener("mousedown", slideReady);
-  div_slider.addEventListener("touchstart", slideReady);
+  var div_resizable = createResizableElement();
+  var div_slider = createSliderElement();
+
   window.addEventListener("mouseup", slideFinish);
   window.addEventListener("touchstop", slideFinish);
   window.addEventListener("resize", resizeElements);
-  // Create resizable Container
-  var div_resizable = document.createElement("DIV");
-  div_container.appendChild(div_resizable);
-  div_resizable.style.position = "relative";
-  div_resizable.style.width = div_regular.offsetWidth + "px";
-  div_resizable.style.height = div_regular.offsetHeight - 4 + "px";
-  div_resizable.style.zIndex = "2";
-  // Create Image for resizable container
-  var img = new Image();
-  img.src = "/src/TKopie.jpg";
-  img.style.height = div_resizable.style.height;
-  img.style.clipPath =
-    "inset(0px 0px 0px " + div_regular.offsetWidth / 2 + "px)";
-  div_resizable.appendChild(img);
 
+  // creates a sliding control to handle image comparison
+  function createSliderElement() {
+    var slider = document.createElement("div");
+    slider.id = "div-slider";
+    slider.setAttribute("class", "slider-container");
+
+    slider.style.left =
+      (div_regular.offsetWidth - slider.offsetWidth / 2) / 2 + "px";
+    slider.style.top = cWidth / 4 + "px";
+    slider.style.zIndex = "3";
+
+    var image = new Image();
+    image.src = "/src/slider.jpg";
+    image.setAttribute("class", "comp-slider");
+    slider.appendChild(image);
+    slider.addEventListener("mousedown", slideReady);
+    slider.addEventListener("touchstart", slideReady);
+
+    div_container.appendChild(slider);
+
+    return slider;
+  }
+  // creates a fixed element with 1st image
+  function createFixedElement() {
+    var fixed = document.createElement("div");
+    fixed.setAttribute("id", "div-regular");
+    fixed.setAttribute("class", "img-container");
+    var image = new Image();
+    image.src = "/src/Terminator.jpg";
+    image.setAttribute("class", "comp-img");
+    image.style.clipPath = "inset(0px 1px 0px 0px)";
+
+    fixed.appendChild(image);
+    div_container.appendChild(fixed);
+
+    return fixed;
+  }
+  // creates a resizable container containung the 2nd image
+  function createResizableElement() {
+    var resizable = document.createElement("div");
+    resizable.style.position = "relative";
+    resizable.style.width = div_regular.offsetWidth + "px";
+    resizable.style.height = div_regular.offsetHeight - 4 + "px";
+    resizable.style.zIndex = "2";
+
+    img.src = "/src/TKopie.jpg";
+    //img.style.height = resizable.style.height;
+    // doesnt work on frist load
+    // image loads after elements
+    // todo: find out img height dynamically and set value
+    // info: retarded html/css page load layout shit
+    // set fixed height value for all sites and check device specific....
+    img.style.height = "247px";
+    img.style.clipPath =
+      "inset(0px 0px 0px " + (div_regular.offsetWidth + 40) / 2 + "px)";
+    resizable.appendChild(img);
+    div_container.appendChild(resizable);
+
+    return resizable;
+  }
   function slideReady(e) {
     e.preventDefault();
     clicked = 1;
@@ -60,12 +101,10 @@ function compareImages() {
     x = x - window.pageXOffset;
     return x;
   }
-
   function slide(x) {
-    div_slider.style.left = x - 15 + "px";
+    div_slider.style.left = x - 20 + "px"; // TODO: calc offset value properly
     img.style.clipPath = "inset(0px 0px 0px " + x + "px)";
   }
-
   function resizeElements() {
     // set slider pos
     cWidth = div_regular.offsetWidth;
@@ -82,4 +121,5 @@ function compareImages() {
   }
 }
 
+// call function
 compareImages();
